0da5a88d27e9cf1464b24d108760cd8100733623
KsSoldatova <ks.gum19@gmai.com>
2022-12-03 00:13:35 +0400

    create list and dir of copies

--- task3.py
+++ task3.py
@@ -9,3 +9,10 @@ from email import generator
 def get_element(class_name: str) -> generator:
     for file_name in os.listdir(os.path.join("dataset", class_name)):
         yield file_name
+
+
+def create_randomname_file(annotation_name: str, dir_copy: str) -> None:
+    file_number = list(range(10001))
+    random.shuffle(file_number)
+    counter = 1
+    create_dir(dir_copy)
