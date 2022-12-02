5ffd1a218d9fb8c61bc27462366375633dba1aa4
KsSoldatova <ks.gum19@gmai.com>
2022-12-03 01:37:17 +0400

    add copies to dir and create annotation + run

--- task3.py
+++ task3.py
@@ -16,3 +16,16 @@ def create_randomname_file(annotation_name: str, dir_copy: str) -> None:
     random.shuffle(file_number)
     counter = 1
     create_dir(dir_copy)
+    for dataset_class in os.listdir("dataset"):
+        for file_name in get_element(dataset_class):
+            shutil.copy(os.path.join(os.path.join("dataset", dataset_class), file_name),
+                        os.path.join(dir_copy, f"{file_number[counter]}.jpg"))
+
+            with open(os.path.join(dir_copy, annotation_name), mode="a", encoding="UTF-16", newline='') as file:
+                file_writer = csv.writer(file, delimiter=",")
+                file_writer.writerow([f"{file_number[counter]}.jpg", dataset_class])
+            counter += 1
+
+
+def run3(annotation_name: str, dir_copy: str) -> None:
+    create_randomname_file(annotation_name, dir_copy)
