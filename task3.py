d23b3533db2c6777833cb021b3692199315a60d1
KsSoldatova <ks.gum19@gmai.com>
2022-12-02 20:36:02 +0400

    some fix (PEP8)

--- task3.py
+++ task3.py
@@ -5,7 +5,7 @@ import shutil
 import csv
 from email import generator
 
-def get_element(class_name:str)->generator:
-    '''This function return us list of names in dataset class'''
+
+def get_element(class_name: str) -> generator:
     for file_name in os.listdir(os.path.join("dataset", class_name)):
         yield file_name
